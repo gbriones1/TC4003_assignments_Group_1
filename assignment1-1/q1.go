ce18c30ea41684611f286c935c51bbe71f704e8d
Juan Francisco Corral Stenner <ashracc@hotmail.com>
2019-08-31 18:02:56 -0500

    modified to meet best practices

--- assignment1-1/q1.go
+++ assignment1-1/q1.go
@@ -2,28 +2,15 @@ package cos418_hw1_1
 
 import (
 	"fmt"
-	"sort"
 	"io/ioutil"
-	"strings"
 	"regexp"
+	"sort"
+	"strings"
 )
 
-// Find the top K most common words in a text document.
-// 	path: location of the document
-//	numWords: number of words to return (i.e. k)
-//	charThreshold: character threshold for whether a token qualifies as a word,
-//		e.g. charThreshold = 5 means "apple" is a word but "pear" is not.
-// Matching is case insensitive, e.g. "Orange" and "orange" is considered the same word.
-// A word comprises alphanumeric characters only. All punctuations and other characters
-// are removed, e.g. "don't" becomes "dont".
-// You should use `checkError` to handle potential errors.
-
 func topWords(path string, numWords int, charThreshold int) []WordCount {
-	// TODO: implement me
-	// HINT: You may find the `strings.Fields` and `strings.ToLower` functions helpful
-	// HINT: To keep only alphanumeric characters, use the regex "[^0-9a-zA-Z]+"
 	// Reads the document
-	text,err := ioutil.ReadFile(path)
+	text, err := ioutil.ReadFile(path)
 	checkError(err)
 	// Gets all the words
 	words := strings.Fields(string(text))
