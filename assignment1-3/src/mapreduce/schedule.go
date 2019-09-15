bf423c5205a278993536f309b4e6bf434cdf0c0c
Gabriel Briones <gabriel.briones.sayeg@intel.com>
2019-09-14 20:14:43 -0500

    Add distributed schedule for mapreduce
    Signed-off-by: Gabriel Briones <gabriel.briones.sayeg@intel.com>

--- assignment1-3/src/mapreduce/schedule.go
+++ assignment1-3/src/mapreduce/schedule.go
@@ -1,5 +1,10 @@
 package mapreduce
 
+import (
+	"net/rpc"
+	"sync"
+)
+
 // schedule starts and waits for all tasks in the given phase (Map or Reduce).
 func (mr *Master) schedule(phase jobPhase) {
 	var ntasks int
@@ -22,5 +27,29 @@ func (mr *Master) schedule(phase jobPhase) {
 	//
 	// TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
 	//
+	var wg sync.WaitGroup
+	for n := 0; n < ntasks; n++ {
+		var args DoTaskArgs
+		args.Phase = phase
+		args.File = mr.files[n]
+		args.JobName = mr.jobName
+		args.TaskNumber = n
+		args.NumOtherPhase = nios
+		wg.Add(1)
+		go func(){
+			defer wg.Done()
+			workerName := <- mr.registerChannel
+			debug("Starting worker: %v for task: %v\n", workerName, args.TaskNumber)
+			c, err := rpc.Dial("unix", workerName)
+			checkError(err)
+			// debug("Client %v\n", c)
+			c.Call("Worker.DoTask", args, struct{}{})
+			go func(){
+				mr.registerChannel <- workerName
+				debug("Registered again worker: %v\n", workerName)
+			}()
+		}()
+	}
+	wg.Wait()
 	debug("Schedule: %v phase done\n", phase)
 }
