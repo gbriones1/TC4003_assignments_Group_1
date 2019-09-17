522862910f0171e07e9597d35c7fa76c516670d7
Gabriel Briones <gabriel.briones.sayeg@intel.com>
2019-09-16 22:30:24 -0500

    Add fault tolerance for workers in mapreduce 1-2
    Signed-off-by: Gabriel Briones <gabriel.briones.sayeg@intel.com>

--- assignment1-2/src/mapreduce/common_map.go
+++ assignment1-2/src/mapreduce/common_map.go
@@ -17,48 +17,23 @@ func doMap(
 	nReduce int, // the number of reduce task that will be run ("R" in the paper)
 	mapF func(file string, contents string) []KeyValue,
 ) {
-	// TODO:
-	// You will need to write this function.
-	// You can find the filename for this map task's input to reduce task number
-	// r using reduceName(jobName, mapTaskNumber, r). The ihash function (given
-	// below doMap) should be used to decide which file a given key belongs into.
-	//
-	// The intermediate output of a map task is stored in the file
-	// system as multiple files whose name indicates which map task produced
-	// them, as well as which reduce task they are for. Coming up with a
-	// scheme for how to store the key/value pairs on disk can be tricky,
-	// especially when taking into account that both keys and values could
-	// contain newlines, quotes, and any other character you can think of.
-	//
-	// One format often used for serializing data to a byte stream that the
-	// other end can correctly reconstruct is JSON. You are not required to
-	// use JSON, but as the output of the reduce tasks *must* be JSON,
-	// familiarizing yourself with it here may prove useful. You can write
-	// out a data structure as a JSON string to a file using the commented
-	// code below. The corresponding decoding functions can be found in
-	// common_reduce.go.
-	//
-	//   enc := json.NewEncoder(file)
-	//   for _, kv := ... {
-	//     err := enc.Encode(&kv)
-	//
-	// Remember to close the file after you have written all the values!
-	// Use checkError to handle errors.
 	// debug("Start doMap function: %v %v %v %v\n",jobName,mapTaskNumber,inFile,nReduce)
+	value, err := ioutil.ReadFile(inFile)
+	checkError(err)
+	result := mapF(inFile, string(value))
+	encoders := make(map[uint32]*json.Encoder)
 	for r := 0; r < nReduce; r++ {
 		outFile := reduceName(jobName, mapTaskNumber, r)
 		// debug("outFile: %v\n", outFile)
-		value, err := ioutil.ReadFile(inFile)
-		checkError(err)
-		result := mapF(inFile, string(value))
 		fileWriter, err := os.Create(outFile)
 		checkError(err)
 		defer fileWriter.Close()
-		enc := json.NewEncoder(fileWriter)
-		for i := range result {
-			err := enc.Encode(&result[i])
-			checkError(err)
-		}
+		encoders[uint32(r)] = json.NewEncoder(fileWriter)
+	}
+	for i := range result {
+		r := ihash(result[i].Key) % uint32(nReduce)
+		err := encoders[r].Encode(&result[i])
+		checkError(err)
 	}
 }
 
